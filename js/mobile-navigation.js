70361c50b3cab687c015e66adc57d3ac1024359b
Patrik-RaS <patrik.pato.madarcik@gmail.com>
2026-03-12 11:04:23 +0100

    final changes and updates.
    Cleaning the code
    
    Applying some fonts
    
    fixing logo sizes and positions
    
    Adding phone responsiveness

--- js/mobile-navigation.js
+++
@@ -1,8 +0,0 @@
-const iconElement = document.querySelector(".menu-icon");
-const ul = document.querySelector(".ulul");
-
-iconElement.addEventListener("click", toggleMenu);
-
-function toggleMenu(){
-    ul.classList.toggle("active");
-}
